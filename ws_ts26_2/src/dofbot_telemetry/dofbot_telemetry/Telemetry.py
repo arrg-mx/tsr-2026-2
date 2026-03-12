83bb8e79d0a20c089f3c39851222549aeed9b11a
Felipe Rivas <rivascf@gmail.com>
2026-03-12 14:49:44 -0600

    Clase del dia 120326

---
+++ ws_ts26_2/src/dofbot_telemetry/dofbot_telemetry/Telemetry.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from dofbot_intefaces.msg import Telemetry
+
+class TelemetryNode(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self._telemetry_pub = self.create_publisher(Telemetry, "/telemetry", 10)
+        self._telemetry_timer  = self.create_timer(1.0, self._on_telemetry_clbk)
+        self.get_logger().info(f"Nodo [{node_name}] inicializado")
+
+    def _on_telemetry_clbk(self):
+        telemetry_msg = Telemetry()
+        telemetry_msg.status = "Active"
+        telemetry_msg.pos_x = 0.0
+        telemetry_msg.pos_y = 0.0
+        telemetry_msg.pos_z = 0.0
+
+        self._telemetry_pub.publish(telemetry_msg)
+
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        telemetry_node = TelemetryNode("telemetry_node")
+        rclpy.spin(telemetry_node)
+    except KeyboardInterrupt as e:
+        pass
+    finally:    
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main() 
+
+   
