e82e0e4410e6a66b1b69a430da182f71170811e9
Felipe Rivas <rivascf@gmail.com>
2026-06-08 20:07:29 -0600

    Actualización clase extra 080626.

--- ws_ts26_2/src/dofbot_telemetry/dofbot_telemetry/robot_telem.py
+++ ws_ts26_2/src/dofbot_telemetry/dofbot_telemetry/robot_telem.py
@@ -6,7 +6,7 @@ from rclpy.node import Node
 # de ROS2 diagnistic_msgs: KeyValue, DiagnosticArray
 from diagnostic_msgs.msg import KeyValue, DiagnosticStatus, DiagnosticArray
 # importamos la(s) librerias externas
-from arrg_utils.sysinfo import SysInfo as si
+from .telem_utils.sysinfo import SysInfo
 
 class DofbotTelemNode(Node):
     def __init__(self, node_name):
@@ -17,38 +17,65 @@ class DofbotTelemNode(Node):
             "/diagnostics",
             10
         )
-
+        self._diag_arr = DiagnosticArray()
+        self.__telem_report = SysInfo()
         self.__diag_timer = self.create_timer(5.0, self._on_diag_timer)
 
         self.get_logger().info(f"{node_name} inicializado...")
 
-    def _on_diag_timer(self):
-        response = DiagnosticArray()
-        # definicion del header
-        response.header.frame_id = ""
-        response.header.stamp = self.get_clock().now().to_msg()
-        diag_data = si.get_system_report()
-        status = list(DiagnosticStatus)
-        Status_item = DiagnosticStatus()
-        Status_item.name = diag_data['host']
-        Status_item.hardware_id = diag_data['platform']['additional_info']['pretty_name']
-        Status_item.message = ""
-        Status_item.level = DiagnosticStatus.OK
-        valores = list(KeyValue)
-        i = 0
-        # Lectura de los CPUs
-        for cpu_stat in diag_data['cpu_stats']:
-            key_value = KeyValue()
-            key_value.key = str(i)
-            key_value._value = cpu_stat[str(i)]['usaged']
-            valores.append(key_value)
-            i +=1
-        # Lectura de la RAM
-        key_value = KeyValue()
-        key_value.key = 'ram'
-        key_value.value = diag_data['ram']['used']
+    def _find_key_recursive(self, data, target_key):
+        """
+        Busca una clave de forma recursiva dentro de diccionarios o listas anidadas.
+        Devuelve el valor si lo encuentra, o None si no existe.
+        """
+        if isinstance(data, dict):
+            if target_key in data:
+                return data[target_key]
+            for key, value in data.items():
+                result = self._find_key_recursive(value, target_key)
+                if result is not None:
+                    return result
+        elif isinstance(data, list):
+            for item in data:
+                result = self._find_key_recursive(item, target_key)
+                if result is not None:
+                    return result
+        return None
 
-        status.append(Status_item)
+
+    def _on_diag_timer(self):
+        # 0. Obtencion del reporte de arrg_utils.sysinfo
+        # raw_report = si.get_system_report()
+        raw_report = self.__telem_report.get_system_report()
+        # 1. Extraemos las secciones con la función de búsqueda
+        cpu_stats = self._find_key_recursive(raw_report, 'cpu_stats')
+        disk = self._find_key_recursive(raw_report, 'disk')
+        ram = self._find_key_recursive(raw_report, 'ram')
+        ip = self._find_key_recursive(raw_report, 'ip')
+        ros = self._find_key_recursive(raw_report, 'ros')
+        # 2. Ajustamos el contenedor del mensaje de ROS 2
+        # DiagnosticsArray
+        self._diag_arr.header.stamp = self.get_clock().now().to_msg()
+        status_list = []
+
+        # 3. Procesamos las secciones del reporte y las incluimos en el mensaje
+        # formato de salida
+        if isinstance(cpu_stats, list):
+            for core in cpu_stats:
+                if isinstance(core, dict) and core.get('type') == 'core':
+                    label = core.get('label', 'Unknown_Core')
+                    usage = core.get('usage')
+
+                    if usage is not None:
+                        cpu_status = DiagnosticStatus()
+                        cpu_status.level = DiagnosticStatus.OK
+                        cpu_status.name = f"cpu_core_{label}"
+                        cpu_status.message = f"Métricas por núcleo del CPU({label})"
+                        cpu_status.hardware_id = f"{label}"
+                        cpu_status.values.append(KeyValue(
+                            key='usage',
+                            value=str(usage)
+                        ))
 
 def init_node(args=None):
     rclpy.init(args=args)
